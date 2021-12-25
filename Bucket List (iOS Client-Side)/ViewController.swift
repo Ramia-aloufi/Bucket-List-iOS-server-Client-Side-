0c745c1b93d0c7e447448b01c26531ac6e6bf71f
RAM111111 <r.r.99@windowslive.com>
2021-12-25 18:56:28 +0300

    Initial Commit

---
+++ Bucket List (iOS Client-Side)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bucket List (iOS Client-Side)
+//
+//  Created by R on 21/05/1443 AH.
+//  Copyright © 1443 R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
